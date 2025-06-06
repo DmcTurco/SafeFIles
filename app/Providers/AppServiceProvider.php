2fdec8d245572efedaa37a04a15ac87073703c94
David Moises Mamani CHino <moisesanibal@hotmail.com>
2025-06-06 11:07:33 -0500

    inicio

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Providers;
+
+use App\MyApp;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Request;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        $requestUri = $this->app->request->getRequestUri();
+        Request::macro('routeType', function () use ($requestUri) {
+            if (preg_match("#^/" . MyApp::ADMINS_SUBDIR . "/#", $requestUri)) {
+                return MyApp::ADMINS_SUBDIR;
+            } else if (preg_match("#^/" . MyApp::COMPANY_SUBDIR . "/#", $requestUri)) {
+                return MyApp::COMPANY_SUBDIR;
+            } else if (preg_match("#^/" . MyApp::COMPANY_SUBDIR . "/#", $requestUri)) {
+                return MyApp::EMPLOYEE_SUBDIR;
+            } else {
+                return null;
+            }
+        });
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Paginator::useBootstrap();
+    }
+}
